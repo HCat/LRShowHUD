83ddf69f19f42c31bfc45c0420baebbd1bab1797
hcat-89 <375348234@qq.com>
2017-06-19 23:54:10 +0800

    更新到显示菊花样式

--- LRShowHUDDemo/LRShowHUDDemo/LRShowHUD/LRShowHUD.h
+++ LRShowHUDDemo/LRShowHUDDemo/LRShowHUD/LRShowHUD.h
@@ -5,8 +5,7 @@
 //  Copyright © 2017年 hcat-89. All rights reserved.
 //
 //----------------------------------------------------------------------------------
-//  1、基于 MBProgressHUD 1.9.2版本封装（支持横竖屏），增加了操作成功、失败的打勾打叉动画，
-//    以及提供了一种可能适用于祝贺类的弹框提示所需的撒花（碎花粒子效果）效果，可酌情使用。
+//  1、基于 MBProgressHUD 最新版本封装（支持横竖屏）
 //    当前扩展的 GitHub 地址：https://github.com/HCat/LRShowHUD
 //  2、当前版本：1.0.0
 //  3、如果在使用过程中有问题或者建议可以随时联系我：375348234@qq.com 或者 GitHub issue
@@ -18,32 +17,42 @@
 #import <Foundation/Foundation.h>
 #import <MBProgressHUD.h>
 
+typedef NS_ENUM(NSInteger, WJHUDLoadingProgressStyle) {
+    /* 开扇型加载进度 */
+    WJHUDLoadingProgressStyleDeterminate,
+    /* 横条加载进度 */
+    WJHUDLoadingStyleDeterminateHorizontalBar,
+    /* 环形加载进度 */
+    WJHUDLoadingStyleAnnularDeterminate,
+};
+
+
 
 /* 配置MBProgressHUD中的参数.主要是基本属性.包括字体大小，颜色以及其他配置选项 */
-typedef void(^ConfigShowHUDBlock)(MBProgressHUD *hud);
+typedef void(^ConfigShowHUDBlock)(MBProgressHUD * _Nullable hud);
 
 /* 配置自定义视图，视图赋值给MBProgressHUD.customView属性 */
-typedef UIView *(^ConfigShowHUDCustomViewBlock)();
+typedef UIView * _Nullable (^ConfigShowHUDCustomViewBlock)();
 
 
 @interface LRShowHUD : NSObject
 
-@property(nonatomic,strong,readonly) MBProgressHUD   *hud;
+@property(nonatomic,strong,nonnull,readonly) MBProgressHUD *hud;
 
 #pragma mark - 显示纯文本信息
 /**
- 显示纯文本信息,并且在几秒之后消失,展示在keyWindow层上
+ 显示纯文本信息的HUD,并且在几秒之后消失,展示在keyWindow层上
  
  @param text        提示信息
  @param duration    持续多久时间之后隐藏
  */
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration;
 
 
 /**
- 显示纯文本信息,并且在几秒之后消失,已经进行默认配置，如果需要特殊配置，请用
+ 显示纯文本信息的HUD,并且在几秒之后消失,已经进行默认配置，如果需要特殊配置，请用
  + (void)showTextOnly:(NSString *)text
              duration:(NSTimeInterval)duration
                inView:(UIView *)view
@@ -54,13 +63,14 @@ typedef UIView *(^ConfigShowHUDCustomViewBlock)();
  @param view        展示的View 如果传nil，则展示在keyWindow层上
  */
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration
-              inView:(UIView *)view;
+              inView:(nullable UIView *)view;
+
 
 
 /**
- 显示纯文本信息，并且在几秒之后消失
+ 显示纯文本信息的HUD，并且在几秒之后消失
  
  @param text        提示信息
  @param duration    持续多久时间之后隐藏
@@ -68,10 +78,83 @@ typedef UIView *(^ConfigShowHUDCustomViewBlock)();
  @param config      MBProgressHUD的属性配置，默认在LRShowHUD.m中有进行配置，如果需要另外配置则在block中进行配置
  */
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration
-              inView:(UIView *)view
-              config:(ConfigShowHUDBlock)config;
+              inView:(nullable UIView *)view
+              config:(nullable ConfigShowHUDBlock)config;
+
+
+
+/**
+ 初始化纯文本信息的HUD，返回LRShowHUD, 不会自动消失，需要在你需要移除的时候调用 hideAnimated: 等移除方法
+ 
+ @param text        提示信息
+ @param view        展示的View 如果传nil，则展示在keyWindow层上
+ @param config      MBProgressHUD的属性配置，默认在LRShowHUD.m中有进行配置，如果需要另外配置则在block中进行配置
+ */
++ (nonnull instancetype)showTextOnly:(nonnull NSString *)text
+                      inView:(nullable UIView *)view
+                      config:(nullable ConfigShowHUDBlock)config;
+
+#pragma mark - 显示菊花加载的HUD
+
+/**
+ 显示带菊花提示信息的HUD，并且在几秒之后消失
+ 
+ @param text        提示信息 备注:text可为空，如果为空则只显示菊花加载
+ @param duration    持续多久时间之后隐藏
+ */
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration;
+
+
+
+/**
+ 显示带菊花提示信息的HUD，并且在几秒之后消失
+ 
+ @param text        提示信息 备注:text可为空，如果为空则只显示菊花加载
+ @param duration    持续多久时间之后隐藏
+ @param view        展示的View 如果传nil，则展示在keyWindow层上
+ */
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration
+                     inView:(nullable UIView *)view;
+
+
+
+/**
+ 显示带菊花提示信息的HUD，并且在几秒之后消失
+ 
+ @param text        提示信息 备注:text可为空，如果为空则只显示菊花加载
+ @param duration    持续多久时间之后隐藏
+ @param view        展示的View 如果传nil，则展示在keyWindow层上
+ @param config      MBProgressHUD的属性配置，默认在LRShowHUD.m中有进行配置，如果需要另外配置则在block中进行配置
+ */
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration
+                     inView:(nullable UIView *)view
+                     config:(nullable ConfigShowHUDBlock)config;
+
+
+
+
+/**
+初始化纯文本信息的HUD，返回LRShowHUD, 不会自动消失，需要在你需要移除的时候调用 hideAnimated: 等移除方法
+
+@param text        提示信息
+@param view        展示的View 如果传nil，则展示在keyWindow层上
+@param config      MBProgressHUD的属性配置，默认在LRShowHUD.m中有进行配置，如果需要另外配置则在block中进行配置
+*/
++ (nonnull instancetype)showActivityLoading:(nullable NSString *)text
+                                     inView:(nullable UIView *)view
+                                     config:(nullable ConfigShowHUDBlock)config;
+
+
+#pragma mark - 显示加载进度的HUD
+
+
+
+
 
 
 #pragma mark - 隐藏
--- LRShowHUDDemo/LRShowHUDDemo/LRShowHUD/LRShowHUD.m
+++ LRShowHUDDemo/LRShowHUDDemo/LRShowHUD/LRShowHUD.m
@@ -35,36 +35,182 @@
 
 #pragma mark - 
 
-- (void)setUpCommonConfig{
+- (void)setUpCommonConfig:(NSString *)text{
 
+    if (text && text.length > 0) {
+        
+        if (text.length > 10) {
+            _hud.detailsLabel.text = text;
+            _hud.detailsLabel.font = [UIFont fontWithName:@"STHeitiSC-Medium" size:14];
+        }else{
+            _hud.label.text = text;
+            _hud.label.font = [UIFont fontWithName:@"STHeitiSC-Medium" size:14];
+        }
     
-
+    }
+    
+    _hud.margin = 10.0f;
+    _hud.backgroundView.style = MBProgressHUDBackgroundStyleSolidColor;
+    _hud.backgroundView.color = [UIColor clearColor];
+    _hud.bezelView.layer.cornerRadius = 3.f;
+    _hud.bezelView.color = [[UIColor blackColor] colorWithAlphaComponent:0.5f];
 
 }
 
 #pragma mark - 显示纯文本信息
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration{
-    [self showTextOnly:text duration:duration inView:nil];
+    [self showTextOnly:text duration:duration inView:nil config:nil];
 
 }
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration
-              inView:(UIView *)view{
+              inView:(nullable UIView *)view{
     
     [self showTextOnly:text duration:duration inView:view config:nil];
 
 }
 
-+ (void)showTextOnly:(NSString *)text
++ (void)showTextOnly:(nonnull NSString *)text
             duration:(NSTimeInterval)duration
-              inView:(UIView *)view
-              config:(ConfigShowHUDBlock)config{
+              inView:(nullable UIView *)view
+              config:(nullable ConfigShowHUDBlock)config{
+
+    if (view == nil) {
+        view = [UIApplication sharedApplication].keyWindow;
+    }
+    
+    LRShowHUD *showHud     = [[LRShowHUD alloc] initWithView:view];
+    showHud.hud.mode = MBProgressHUDModeText;
+    
+    //默认配置
+    [showHud setUpCommonConfig:text];
+    
+    // 配置额外的参数
+    if (config) {
+        config(showHud.hud);
+    }
+    
+    // 显示
+    [showHud.hud showAnimated:YES];
+
+    // 延迟duration后消失
+    [showHud hideAnimated:YES afterDelay:duration];
+}
+
++ (nonnull instancetype)showTextOnly:(nonnull NSString *)text
+                              inView:(nullable UIView *)view
+                              config:(nullable ConfigShowHUDBlock)config{
+    
+    if (view == nil) {
+        view = [UIApplication sharedApplication].keyWindow;
+    }
+    
+    LRShowHUD *showHud     = [[LRShowHUD alloc] initWithView:view];
+    showHud.hud.mode = MBProgressHUDModeText;
+    
+    //默认配置
+    [showHud setUpCommonConfig:text];
+    
+    // 配置额外的参数
+    if (config) {
+        config(showHud.hud);
+    }
+    
+    // 显示
+    [showHud.hud showAnimated:YES];
+
+    return showHud;
+    
+}
+
+#pragma mark - 显示菊花加载的HUD
+
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration{
+
+    [self showActivityLoading:text duration:duration inView:nil config:nil];
+
+}
+
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration
+                     inView:(nullable UIView *)view{
+    
+    [self showActivityLoading:text duration:duration inView:view config:nil];
+
+}
 
 
++ (void)showActivityLoading:(nullable NSString *)text
+                   duration:(NSTimeInterval)duration
+                     inView:(nullable UIView *)view
+                     config:(nullable ConfigShowHUDBlock)config{
 
+    LRShowHUD *showHud     = [[LRShowHUD alloc] initWithView:view];
+    showHud.hud.mode = MBProgressHUDModeIndeterminate;
+    
+    //默认配置
+    [showHud setUpCommonConfig:text];
+    
+    // 配置额外的参数
+    if (config) {
+        config(showHud.hud);
+    }
+    
+    // 显示
+    [showHud.hud showAnimated:YES];
+    
+    // 延迟duration后消失
+    [showHud hideAnimated:YES afterDelay:duration];
+
+}
+
++ (nonnull instancetype)showActivityLoading:(nullable NSString *)text
+                                     inView:(nullable UIView *)view
+                                     config:(nullable ConfigShowHUDBlock)config{
+    
+    LRShowHUD *showHud     = [[LRShowHUD alloc] initWithView:view];
+    showHud.hud.mode = MBProgressHUDModeIndeterminate;
+    
+    //默认配置
+    [showHud setUpCommonConfig:text];
+    
+    // 配置额外的参数
+    if (config) {
+        config(showHud.hud);
+    }
+    
+    // 显示
+    [showHud.hud showAnimated:YES];
+    
+    return showHud;
+
+}
+
+#pragma mark - 显示加载进度的HUD
+
+
+#pragma mark - 隐藏
+
+- (void)hideAnimated:(BOOL)isNeedAnimated{
+
+     [_hud hideAnimated:isNeedAnimated];
+    
+}
+
+
+- (void)hideAnimated:(BOOL)isNeedAnimated afterDelay:(NSTimeInterval)delay{
+
+    [_hud hideAnimated:isNeedAnimated afterDelay:delay];
+
+}
+
+- (void)dealloc
+{
+    NSLog(@"LRShowHUD dealloc");
 }
 
 @end
